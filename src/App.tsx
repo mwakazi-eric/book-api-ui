ad60d66c6c78d2734a05bba541338bc208bdcaef
Sudarshan Shah <sudarshanshah911@gmail.com>
2025-05-04 19:47:11 +0530

    add, edit, and delete APIs completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import AddBook from "./components/AddBook";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/add-book" element={<AddBook />} />
       </Routes>
     </Router>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
-import { getAllBooks } from "../services/book.service";
+import { getAllBooks, updateBook } from "../services/book.service";
 import { BookDto } from "../types/BookDto";
+import EditBook from "./EditBook";
+import DeleteBook from "./DeleteBook";
 
 function Home() {
   const [books, setBooks] = useState<BookDto[]>([]);
 
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [bookToEdit, setBookToEdit] = useState<BookDto | null>(null);
+
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [bookToDelete, setBookToDelete] = useState<BookDto | null>(null);
+
   useEffect(() => {
+    getBooks();
+  }, []);
+
+  const getBooks = () => {
     getAllBooks()
       .then((response) => {
         console.log("response = ", response.data);
@@ -14,7 +26,39 @@ function Home() {
       .catch((error) => {
         console.log("error = ", error);
       });
-  }, []);
+  };
+
+  const handleEdit = (book: BookDto) => {
+    setBookToEdit(book);
+    setIsEditDialogOpen(true);
+  };
+
+  const handleOnClose = () => {
+    setBookToEdit(null);
+    setIsEditDialogOpen(false);
+  };
+
+  const handleUpdateBook = (updatedBook: BookDto, bookCoverFile?: File) => {
+    const submitData = new FormData();
+
+    submitData.append("bookDtoJson", JSON.stringify(updatedBook));
+
+    if (bookCoverFile) {
+      submitData.append("file", bookCoverFile);
+    }
+
+    updateBook(submitData, updatedBook.isbn)
+      .then((response) => {
+        console.log("Update book response = ", response);
+        getBooks();
+        handleOnClose();
+      })
+      .catch((error) => {
+        console.log("Error updating book : ", error);
+      });
+  };
+
+  const handleDelete = (book: BookDto) => {};
 
   return (
     <>
@@ -34,10 +78,16 @@ function Home() {
                 <strong>Price</strong>: $ {book.price}
               </p>
               <div className="flex my-2">
-                <button className="flex-1 bg-yellow-600 text-white py-1 px-1 mr-2 rounded-md">
+                <button
+                  className="flex-1 bg-yellow-600 text-white py-1 px-1 mr-2 rounded-md"
+                  onClick={() => handleEdit(book)}
+                >
                   Edit
                 </button>
-                <button className="flex-1 bg-red-600 text-white py-1 px-1 rounded-md">
+                <button
+                  className="flex-1 bg-red-600 text-white py-1 px-1 rounded-md"
+                  onClick={() => handleDelete(book)}
+                >
                   Delete
                 </button>
               </div>
@@ -45,6 +95,17 @@ function Home() {
           ))}
         </div>
       </div>
+
+      {bookToEdit && (
+        <EditBook
+          book={bookToEdit}
+          isOpen={isEditDialogOpen}
+          onClose={handleOnClose}
+          onSubmit={handleUpdateBook}
+        />
+      )}
+
+      {bookToDelete && <DeleteBook />}
     </>
   );
 }
